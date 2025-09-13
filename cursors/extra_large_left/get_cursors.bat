4a2529ca5b4c79740dfbe0afc211d6b3a72e02fb
russiandesman <des.maryin@googlemail.com>
2025-09-13 17:20:20 +0200

    Unify cursor names
    Signed-off-by: russiandesman <des.maryin@googlemail.com>

--- cursors/extra_large_left/get_cursors.bat
+++
@@ -1,7 +0,0 @@
-copy "%SystemRoot%\\cursors\\aero_unavail_xl.cur" aero_unavail_xl.cur
-copy "%SystemRoot%\\cursors\\aero_ns_xl.cur" aero_ns_xl.cur
-copy "%SystemRoot%\\cursors\\aero_ew_xl.cur" aero_ew_xl.cur
-copy "%SystemRoot%\\cursors\\aero_nwse_xl.cur" aero_nwse_xl.cur
-copy "%SystemRoot%\\cursors\\aero_nesw_xl.cur" aero_nesw_xl.cur
-copy "%SystemRoot%\\cursors\\aero_move_xl.cur" aero_move_xl.cur
-copy "%SystemRoot%\\cursors\\aero_up_xl.cur" aero_up_xl.cur
--- cursors/large_left/get_cursors.bat
+++
@@ -1,7 +0,0 @@
-copy "%SystemRoot%\\cursors\\aero_unavail_l.cur" aero_unavail_l.cur
-copy "%SystemRoot%\\cursors\\aero_ns_l.cur" aero_ns_l.cur
-copy "%SystemRoot%\\cursors\\aero_ew_l.cur" aero_ew_l.cur
-copy "%SystemRoot%\\cursors\\aero_nwse_l.cur" aero_nwse_l.cur
-copy "%SystemRoot%\\cursors\\aero_nesw_l.cur" aero_nesw_l.cur
-copy "%SystemRoot%\\cursors\\aero_move_l.cur" aero_move_l.cur
-copy "%SystemRoot%\\cursors\\aero_up_l.cur" aero_up_l.cur
